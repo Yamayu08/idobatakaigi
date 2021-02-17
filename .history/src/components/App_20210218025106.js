09189ed58c3c104d8623ae00c38dd67045c1da45
uya <yamayu0088@gmail.com>
2021-02-18 03:00:18 +0900

    create setNAme

--- .history/src/components/App_20210218025106.js
+++ .history/src/components/App_20210218025106.js
@@ -2,8 +2,7 @@ import React, { useState } from 'react';
 import SignIn from './SignIn';
 
 export default () => {
-  const [name, setName] = useState('');
+  const = [name, setName] = useState('');
   console.log({ name });
-
-  return <SignIn setName={setName} />;
+  return <SignIn />;
 };
